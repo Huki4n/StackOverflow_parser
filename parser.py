9732483447f4e7f083975ee80e3978c8510ec5f3
K1berman <Kiberman73@gmail.com>
2023-12-27 22:05:19 +0300

    alpha build

--- parser.py
+++ parser.py
@@ -1,7 +1,29 @@
 from flask import Flask
+import requests
+from getpass import getpass
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 
 @app.route("/stackoverflow")
 def hello_world():
   return "<p style = 'font-size:2rem;color:red;'>Hello, world!<p>"
+
+login_url = 'https://stackoverflow.com/users/login'
+login_data = {
+    'email': "ggag22221@mail.ru",
+    'password': "parsman123"
+}
+
+
+promt = input("Введите проблему: ")
+
+
+session = requests.Session() # запуск сессии
+autorization = session.post(login_url, data=login_data)
+session = session.get(f"https://stackoverflow.com/search?q={promt}")
+
+file = open('site.html', 'w')
+for html in session.text:
+  file.write(html)
