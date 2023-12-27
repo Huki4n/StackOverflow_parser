78e3818a5bd890594a20ab826498ee405ecdb864
hukki <ryurickryurik@yandex.ru>
2023-12-27 23:00:26 +0300

    add function in parser 27.12

--- parser.py
+++ parser.py
@@ -2,10 +2,6 @@ import requests
 #from getpass import getpass
 #from bs4 import BeautifulSoup
 
-def getRequestUrl(request):
-  request_url = "+".join(request.split(" "))
-  return  request_url
-
 def getHTMLfile(request_url):
   login_url = 'https://stackoverflow.com/users/login'
   login_data = {
@@ -15,7 +11,7 @@ def getHTMLfile(request_url):
 
   session = requests.Session()  # запуск сессии
   autorization = session.post(login_url, data=login_data)
-  session = session.get(f"https://stackoverflow.com/search?q={getRequestUrl(request_url)}")
+  session = session.get(f"https://stackoverflow.com/search?q={request_url}")
 
   file = open('site.html', 'w', encoding="utf-8")
   for html in session.text:
