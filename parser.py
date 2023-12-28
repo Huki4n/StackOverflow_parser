e2a9198b4b44e32e4446993780fce215dd061e22
hukki <ryurickryurik@yandex.ru>
2023-12-28 21:21:36 +0300

    delete unusable function

--- parser.py
+++ parser.py
@@ -5,37 +5,30 @@ import os
 
 app = Flask(__name__)
 
-
-
-#Авторизация
+# Авторизация
 login_url = 'https://stackoverflow.com/users/login'
 login_data = {
-    'email': "ggag22221@mail.ru",
-    'password': "parsman123"
+  'email': "ggag22221@mail.ru",
+  'password': "parsman123"
 }
+
 def getAnswersForRequests(request):
-  deleteAnswers()
   session = requests.Session()  # запуск сессии
   autorization = session.post(login_url, data=login_data)  # авторизация
-  session = session.get(f"https://stackoverflow.com/search?q={request}") # получаем страницу со всеми ответами
+  session = session.get(f"https://stackoverflow.com/search?q={request}")  # получаем страницу со всеми ответами
 
-  soup = BeautifulSoup(session.text,"lxml") # форматируем для работы в bs4
-  answers_data = soup.findAll("div",class_="s-post-summary") # находим все ответы по промту
+  soup = BeautifulSoup(session.text, "lxml")  # форматируем для работы в bs4
+  answers_data = soup.findAll("div", class_="s-post-summary")  # находим все ответы по промту
 
   counter = 0
-  for links in answers_data[0:3]: # отделяем 3 ответа и записываем их в HTML
-      find_link = links.find("h3",class_="s-post-summary--content-title").find("a",class_="s-link").get("href") #ищем ссылку на ответ
-      response = requests.get(f"https://stackoverflow.com{find_link}") #переходим на страницу с ответом
-      soup = BeautifulSoup(response.text,"lxml") # форматируем для работы в bs4
-      div = soup.find("div", class_="answer").find("div", class_="s-prose js-post-body") #вырезаем div с ответом
-      counter += 1
-      file = open(f'templates/answer{counter}.html', 'w', encoding="utf-8") #сохраняем в файлы
-      for html in div:
-          file.write(str(html))
+  for links in answers_data[0:3]:  # отделяем 3 ответа и записываем их в HTML
+    find_link = links.find("h3", class_="s-post-summary--content-title").find("a", class_="s-link").get(
+      "href")  # ищем ссылку на ответ
+    response = requests.get(f"https://stackoverflow.com{find_link}")  # переходим на страницу с ответом
+    soup = BeautifulSoup(response.text, "lxml")  # форматируем для работы в bs4
+    div = soup.find("div", class_="answer").find("div", class_="s-prose js-post-body")  # вырезаем div с ответом
 
-
-def deleteAnswers():
-  if os.path.exists(f'templates/answer1.html'):
-    for i in range(1,4):
-      file = open(f'templates/answer{i}.html', 'w', encoding="utf-8") #сохраняем в файлы
-      file.write('')
+    counter += 1
+    file = open(f'templates/answer{counter}.html', 'w', encoding="utf-8")  # сохраняем в файлы
+    for html in div:
+      file.write(str(html))
