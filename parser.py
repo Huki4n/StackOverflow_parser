2195e86752dd92b0faaf41582503eba39a3a2cba
K1berman <Kiberman73@gmail.com>
2023-12-28 16:02:31 +0300

    Build 0.228

--- parser.py
+++ parser.py
@@ -19,11 +19,20 @@ login_data = {
 
 promt = input("Введите проблему: ")
 
-
 session = requests.Session() # запуск сессии
-autorization = session.post(login_url, data=login_data)
-session = session.get(f"https://stackoverflow.com/search?q={promt}")
+autorization = session.post(login_url, data=login_data) # авторизация
+session = session.get(f"https://stackoverflow.com/search?q={promt}") # получаем страницу со всеми ответами
 
-file = open('site.html', 'w')
-for html in session.text:
-  file.write(html)
+soup = BeautifulSoup(session.text,"lxml") # форматируем для работы в bs4
+answers_data = soup.findAll("div",class_="s-post-summary") # находим все ответы по промту
+
+counter = 0
+for links in answers_data[0:3]: # отделяем 3 ответа и записываем их в HTML
+    find_link = links.find("h3",class_="s-post-summary--content-title").find("a",class_="s-link").get("href") #ищем ссылку на ответ
+    response = requests.get(f"https://stackoverflow.com{find_link}") #переходим на страницу с ответом
+    soup = BeautifulSoup(response.text,"lxml") # форматируем для работы в bs4
+    div = soup.find("div", class_="answer").find("div", class_="s-prose js-post-body") #вырезаем div с ответом
+    counter += 1
+    file = open(f'site{counter}.html', 'w', encoding="utf-8") #сохраняем в файлы
+    for html in div:
+        file.write(str(html))
